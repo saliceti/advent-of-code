cc43a9d9bae9ba82199929041750287f8f776f95
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2024-12-19 21:13:44 +0000

    day1 python

---
+++ day1.py
@@ -0,0 +1,27 @@
+
+file = open("day1_input.txt", "r")
+column1 = []
+column2 = []
+total = 0
+
+for line in file:
+  thingy1 = line[0:5]
+  thingy2 = line[8:14]
+
+  column1.append(int(thingy1))
+  column2.append(int(thingy2))
+
+column1.sort()
+column2.sort()
+
+for x in range(0, len(column1)):
+  d = column1[x] - column2[x]
+
+  if d < 0:
+    d = d * -1
+
+  total += d
+
+print(total)
+print(column1[0])
+print(column2[0])
